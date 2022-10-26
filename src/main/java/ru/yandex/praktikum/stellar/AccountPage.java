3ce97cd8d0ceaea05d2cee07e8d0b71f96a8af3c
Artem Borisenko <aprricod@gmail.com>
2022-10-27 00:09:36 +0200

    navigation tests added

--- src/main/java/ru/yandex/praktikum/stellar/AccountPage.java
+++ src/main/java/ru/yandex/praktikum/stellar/AccountPage.java
@@ -1,4 +1,32 @@
 package ru.yandex.praktikum.stellar;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
 public class AccountPage extends BasePage{
+    // инпут имя
+    @FindBy(how = How.XPATH, using = "//input[@name='Name']")
+    private SelenideElement nameInput;
+
+    // линк конструктор
+    @FindBy(how = How.XPATH, using = "//p[text()='Конструктор']")
+    private SelenideElement constructorLink;
+
+    // лого стеллар
+    @FindBy(how = How.XPATH, using = "//div[@class='AppHeader_header__logo__2D0X2']/a")
+    private SelenideElement stellarLogo;
+
+    public void checkIfInputVisible(){
+        nameInput.shouldBe(Condition.visible);
+    }
+
+    public void clickConstructorLink(){
+        constructorLink.click();
+    }
+
+    public void clickStellarLogo(){
+        stellarLogo.click();
+    }
 }
--- src/test/java/tests/UserLoginTest.java
+++ src/test/java/tests/UserLoginTest.java
@@ -1,20 +1,19 @@
 package tests;
 
+import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Step;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
-import ru.yandex.praktikum.stellar.LoginPage;
-import ru.yandex.praktikum.stellar.MainPage;
-import ru.yandex.praktikum.stellar.PasswordRestorePage;
-import ru.yandex.praktikum.stellar.RegisterPage;
+import ru.yandex.praktikum.stellar.*;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.page;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertEquals;
 
 public class UserLoginTest {
 
     static String baseURL = "https://stellarburgers.nomoreparties.site/";
-    static String loginURL = "https://stellarburgers.nomoreparties.site/login";
+    static String profileURL = "https://stellarburgers.nomoreparties.site/account/profile";
 
     String userEmail = "stellar.test1@yopmail.com";
     String userPassword = "123456";
@@ -73,5 +72,58 @@ public class UserLoginTest {
         loginPage.setUserData(userEmail, userPassword);
         loginPage.clickLoginButton();
         mainPage.checkCreateOrderButton();
+    }
+
+    // Переход в личный кабинет
+    @Test
+    public void openUserAccountTest() {
+        MainPage mainPage = page(MainPage.class);
+        LoginPage loginPage = page(LoginPage.class);
+        AccountPage accountPage = page(AccountPage.class);
+        mainPage.clickAccountButton();
+        loginPage.setUserData(userEmail, userPassword);
+        loginPage.clickLoginButton();
+        mainPage.clickAccountButton();
+        accountPage.checkIfInputVisible();
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        assertEquals(profileURL, currentUrl);
+    }
+
+    // Переход из личного кабинета в конструктор
+    @Test
+    public void navigateToConstructorFromUserAccountTest() {
+        MainPage mainPage = page(MainPage.class);
+        LoginPage loginPage = page(LoginPage.class);
+        AccountPage accountPage = page(AccountPage.class);
+        mainPage.clickAccountButton();
+        loginPage.setUserData(userEmail, userPassword);
+        loginPage.clickLoginButton();
+        mainPage.clickAccountButton();
+        accountPage.clickConstructorLink();
+        mainPage.checkCreateOrderButton();
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        assertEquals(baseURL, currentUrl);
+    }
+
+    // Переход из личного кабинета в конструктор по клику на логотип
+    @Test
+    public void navigateToConstructorFromUserAccountByClickOnLogoTest() {
+        MainPage mainPage = page(MainPage.class);
+        LoginPage loginPage = page(LoginPage.class);
+        AccountPage accountPage = page(AccountPage.class);
+        mainPage.clickAccountButton();
+        loginPage.setUserData(userEmail, userPassword);
+        loginPage.clickLoginButton();
+        mainPage.clickAccountButton();
+        accountPage.clickStellarLogo();
+        mainPage.checkCreateOrderButton();
+        String currentUrl = WebDriverRunner.getWebDriver().getCurrentUrl();
+        assertEquals(baseURL, currentUrl);
+    }
+
+    @AfterClass
+    @Step("after")
+    public static void closeDriver() {
+        closeWebDriver();
     }
 }
